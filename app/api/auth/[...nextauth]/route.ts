b4efbff3b1dadb012951ff219c0a306a292e3169
Novojit Saha <n.saha@student.utwente.nl>
2023-08-07 19:11:49 +0200

    - Add auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google';
+
+const authOptions: NextAuthOptions={
+    providers:[
+        GoogleProvider({
+            clientId:process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            authorization:{
+                params:{
+                    prompt:'consent',
+                    access_type:'offline',
+                    response_type:'code'
+                }
+            }
+
+        }),
+    ]
+}
+
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 export default function(){
     return(
+        
         <main>
             <h1>Login</h1>
         </main>
